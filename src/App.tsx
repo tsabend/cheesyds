0416ac60ec2c3ee013d83f77a3ee99b270abca0d
tabend <thomas.abend@disneystreaming.com>
2020-03-23 22:06:09 -0400

    Add firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import GameView from "./features/cards/GameView";
+import { fire } from './app/fire';
 import {
   createMuiTheme,
   withStyles,
@@ -28,46 +29,85 @@ const theme = createMuiTheme({
 });
 
 const styles: (theme: Theme) => StyleRules<string> = theme =>
-  createStyles({
-    root: {
+createStyles({
+  root: {
 
-    },
-    app: {
-      textAlign: "center"
-    },
-    appLogo: {
-      height: "40vmin",
-      pointerEvents: "none",
-      "@media (prefers-reduced-motion: no-preference) ": {
-        animation: "App-logo-float infinite 3s ease-in-out"
-      }
-    },
-    appHeader: {
-      minHeight: "100vh",
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-      fontSize: "calc(10px + 2vmin)"
-    },
-    appLink: {
-      color: "rgb(112, 76, 182)"
+  },
+  app: {
+    textAlign: "center"
+  },
+  appLogo: {
+    height: "40vmin",
+    pointerEvents: "none",
+    "@media (prefers-reduced-motion: no-preference) ": {
+      animation: "App-logo-float infinite 3s ease-in-out"
     }
-  });
+  },
+  appHeader: {
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "calc(10px + 2vmin)"
+  },
+  appLink: {
+    color: "rgb(112, 76, 182)"
+  }
+});
 
 type AppProps = {} & WithStyles<typeof styles>;
 
-const App = ({ classes }: AppProps) => (
-  <MuiThemeProvider theme={theme}>
+const App = ({ classes }: AppProps) => {
+  const [messages, setMessages] = useState(Array<any>())
+
+  useEffect(() => {
+    console.log("USE EFFECT CALLED")
+    /* Create reference to messages in Firebase Database */
+    let messagesRef = fire.database().ref('messages').orderByKey().limitToLast(100);
+    messagesRef.on('child_added', snapshot => {
+        /* Update React state when message is added at Firebase Database */
+        let message = { text: snapshot.val(), id: snapshot.key };
+        setMessages([message].concat(messages));
+      })
+    }, []);
+   const addMessage = (e: any) => {
+     e.preventDefault(); // <- prevent form submit from reloading the page
+     /* Send the message to Firebase */
+     const m = e.target.elements[0].value
+     fire.database().ref('messages').push( m );
+     e.target.elements[0].value = ''; // <- clear the input
+   }
+
+  return (
+    <MuiThemeProvider theme={theme}>
     <CssBaseline />
     <div className={classes.app}>
-      <header className={classes.appHeader}>
-        <p>CheesyD</p>
-        <GameView />
-      </header>
+    <header className={classes.appHeader}>
+    <p>CheesyD</p>
+    <GameView />
+    </header>
     </div>
-  </MuiThemeProvider>
-);
+    <React.Fragment>
+    <form onSubmit={(e) => {
+        addMessage(e);
+      }
+    }>
+
+    <input type="text" />
+    <input type="submit"/>
+    <ul>
+    { /* Render the list of messages */
+      messages.map( message => <li key={message.id}>{message.text}</li> )
+    }
+    </ul>
+    </form>
+    </React.Fragment>
+    </MuiThemeProvider>
+  );
+}
+
+export default withStyles(styles)(App);
 
 // const App = ({ classes }: AppProps) => (
 //   <MuiThemeProvider theme={theme}>
@@ -130,5 +170,3 @@ const App = ({ classes }: AppProps) => (
 //     </div>
 //   </MuiThemeProvider>
 // );
-
-export default withStyles(styles)(App);
---
+++ src/app/fire.ts
@@ -0,0 +1,13 @@
+import * as Firebase from 'firebase';
+require('dotenv').config()
+const config = {
+  apiKey: process.env.FB_API_KEY,
+  authDomain: "cheesyd-c02a3.firebaseapp.com",
+  databaseURL: "https://cheesyd-c02a3.firebaseio.com",
+  projectId: "cheesyd-c02a3",
+  storageBucket: "cheesyd-c02a3.appspot.com",
+  messagingSenderId: process.env.FB_MESSAGE_SENDER_ID,
+  appId: process.env.FB_APP_ID,
+  measurementId: process.env.FB_MEASUREMENT_ID
+};
+export const fire = Firebase.initializeApp(config);
