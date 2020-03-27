e1214d0564ce332d18a16dfa86e8d885c218a835
tabend <thomas.abend@disneystreaming.com>
2020-03-27 15:32:16 -0400

    configure remote state

--- src/app/fire.ts
+++ src/app/fire.ts
@@ -1,14 +1,21 @@
 import * as Firebase from 'firebase';
+import * as functions from 'firebase-functions'
 require('dotenv').config()
+
+const appKey = process.env.FB_API_KEY || functions.config().fb?.api_key;
+const senderId = process.env.FB_MESSAGE_SENDER_ID || functions.config().fb?.message_sender_id;
+const appId = process.env.FB_APP_ID || functions.config().fb?.app_id
+const measurementId = process.env.FB_MEASUREMENT_ID || functions.config().fb?.measurement_id
+
 const config = {
-  apiKey: process.env.FB_API_KEY,
+  apiKey: appKey,
   authDomain: "cheesyd-c02a3.firebaseapp.com",
   databaseURL: "https://cheesyd-c02a3.firebaseio.com",
   projectId: "cheesyd-c02a3",
   storageBucket: "cheesyd-c02a3.appspot.com",
-  messagingSenderId: process.env.FB_MESSAGE_SENDER_ID,
-  appId: process.env.FB_APP_ID,
-  measurementId: process.env.FB_MEASUREMENT_ID
+  messagingSenderId: senderId,
+  appId: appId,
+  measurementId: measurementId
 };
 
 export type FBApp = Firebase.app.App;
